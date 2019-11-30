04a4d4c2a0345095174e2f86ea01ebcc1e8d8805
tr1503 <hanlinye6@gmail.com>
2019-11-30 16:17:05 -0500

    add admin routers

--- routes/adminInfo.js
+++ routes/adminInfo.js
@@ -19,28 +19,63 @@ router.get("/:adminid", checkAdmin, (req, res, next) => {
     });
 });
 
-// get one user's information
-router.get("/:adminid/:username", checkAdmin, (req, res, next) => {
-    User.findOne({username: req.params.username})
-        .then(user => {
-            if (!user) {
-                return res.status.json({
-                    message: "Can't find this user"
-                })
-            }
-            return user._id;
-        })
-        .then(result => {
-            Post.find({authorid: result})
-                .then(posts => {
-                    res.status(200).send(posts);
-                });
+// get one user's posts
+router.get("/:adminid/:username/posts", checkAdmin, (req, res, next) => {
+    Post.find({author: req.params.username})
+        .then(posts => {
+            res.status(200).send(posts);
         })
         .catch(err => {
-            res.status(500).json({
+            return res.status(401).json({
                 error: err
-            })
+            });
         });
+});
+
+// get one user's comments
+router.get("/:adminid/:username/comments", checkAdmin, (req, res, next) => {
+    Comment.find({author: req.params.username})
+            .then(comments => {
+                res.status(200).send(comments);
+            })
+            .catch(err => {
+                return res.status(401).json({
+                    error: err
+                })
+            });
+});
+
+// get daily count of posts
+router.get("/:adminid/serach/:timestamp/posts", checkAdmin, (req, res, next) => {
+    var start = req.params.timestamp;
+    var end = new Date();
+    end.setDate(end.getDate() + 86400000);
+    Post.count({timestamp: {"$gte": start, "$lt": end}})
+        .then(result => {
+            res.status(200).send(result);
+        })
+        .catch(err => {
+            return res.status(401).json({
+                error: err
+            });
+        })
+
+}); 
+
+// get daily count of comments
+router.get("/:adminid/search/:timestamp/comments", checkAdmin, (req, res, next) => {
+    var start = req.params.timestamp;
+    var end = new Date();
+    end.setDate(end.getDate() + 86400000);
+    Comment.count({timestamp: {"$gte": start, "$lt":end}})
+            .then(result => {
+                res.status(200).send(result);
+            })
+            .catch(err => {
+                return res.status(401).json({
+                    error: err
+                });
+            });
 });
 
 module.exports = router;
